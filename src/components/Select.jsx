16e623cad8a6e10cc360edc1b6b42961629eda1e
rushikeshthakur25 <rushikeshthakur32@gmail.com>
2023-11-09 15:28:20 +0530

    add Components

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -16,10 +16,14 @@ function Select({
                 ref={ref}
                 className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-300 duration-200 border border-gray-300 w-full ${className}`}
             >
+                {options?.map((option) => (
+                    <option key={option} value={option} > 
 
+                    </option>
+                ))}
             </select>
         </div>
     )
 }
 
-export default Select
+export default React.forwardRef(Select)
---
+++ src/components/post-form/PostFrom.jsx
@@ -0,0 +1,16 @@
+import React, {useCallback} from 'react'
+import {useForm} from 'react-hook-form'
+import {Button, Input, Select , RTE} from '../index'
+import appwriteService from '../../appwrite/config';
+import { useNavigate } from 'react-router-dom';
+import { UseSelector } from 'react-redux';
+
+function PostFrom() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PostFrom
